a7790d9992aaeafbc230f1b4a658958524dcd2d1
刘泽泓 <liuzehong@liuzehongdeMacBook-Air.local>
2015-02-02 23:35:59 +0800

    Initial Commit

---
+++ c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c
+//
+//  Created by 刘泽泓 on 15-2-2.
+//  Copyright (c) 2015年 刘泽泓. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
